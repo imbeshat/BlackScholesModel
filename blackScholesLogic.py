23175712eb60b71bf500e90004651bd998dfeff4
Imbeshat <md.imbeshat@gmail.com>
2025-03-27 15:48:52 +0530

    modified variables name

--- blackScholesLogic.py
+++ blackScholesLogic.py
@@ -4,16 +4,16 @@ from scipy.stats import norm
 # Black Scholes Logic
   """
   Parameters:
-  s = Stock Price
-  x = Exercise/Strike Price
+  S = Stock (or other underlying) Price
+  K = Exercise/Strike Price
   t = Time to Maturity/Expiration in years
   r = Risk-free Interest Rate
   sigma = Standard deviation of log returns(volatility)
   """
 class BlackScholes:
   def __init__(self, 
-  s, 
-  x, 
-  t, 
-  r, 
-  sigma):
+  S: float, 
+  K: float, 
+  t: float, 
+  r: float, 
+  sigma: float):
